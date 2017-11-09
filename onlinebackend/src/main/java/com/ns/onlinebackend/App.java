6101a3fa96f42ea62e0da1adc3fe40096bea42da
nagraj001 <nagk785@gmail.com>
2017-11-09 17:36:49 +0530

    my project setup was completed

---
+++ onlinebackend/src/main/java/com/ns/onlinebackend/App.java
@@ -0,0 +1,13 @@
+package com.ns.onlinebackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
